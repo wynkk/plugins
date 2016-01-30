70631191c834b2b35e7527da1afd6d7e9e34ba55
Haza <hamzawaqas.programmer@gmail.com>
2016-01-30 20:13:26 +0000

    code lookup added

---
+++ lookup.js
@@ -0,0 +1,35 @@
+var codes = require('../bin/codes').results.collection1;
+var format = require('util').format;
+
+console.log(codes);
+module.exports.findCode = function (disease, cb) {
+    disease = disease ||'';
+    if (!disease) {
+        return cb(null, 'Sorry, I can\'t find the code.');
+    }
+
+    // Cholera
+
+    disease = disease.toLowerCase();
+
+    var isFound = false;
+    var found = [];
+    var foundCodes = [];
+    for (var k = 0, total = codes.length; k < total; k++) {
+        var record = codes[k];
+        if (String(record.diagnosis).toLowerCase().indexOf(disease) >= 0 ||
+            String(record.includes).toLowerCase().indexOf(disease) >= 0) {
+            record.code = record['ICD-10_code'];
+            isFound = true;
+            foundCodes.push(record.diagnosis);
+            found.push(record);
+        }
+    }
+
+    if (!isFound) {
+        return cb(null, 'Sorry, I can\'t find the code.');
+    }
+
+    // `disease`refers to string.
+    return cb(null, format('It could be %s but can also be %s', foundCodes[0], foundCodes.slice(1).join(' or ')));
+};
